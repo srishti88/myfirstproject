60f70067b56d4bdfd5992d96e2219c52737a5b1c
srishti88 <srishti.kalia96@gmail.com>
2017-06-29 11:56:18 +0530

    My first Commit

---
+++ steg.py
@@ -0,0 +1,23 @@
+# from steganography.steganography import Steganography
+# in_image = "po_hero_encoded.jpg"
+# out_image = "po_hero_encoded2.jpg"
+# message = " likes to code"
+# Steganography.encode(in_image,out_image,message)
+# path = "po_hero_encoded2.jpg"
+# print Steganography.decode(path)
+# %a - Abbreviated weekday name
+# %A - Full weekday name
+# %b - Abbreviated month name
+# %B - Full month name
+# %d - Day of the month (01-31)
+# %H - Hour, using a 24-hr clock (0-23)
+# %l - Hour, using a 12-hr clock (01-12)
+# %j - Day of the year (001-366)
+# %m - Month (01-12)
+# %M - Minute
+# from datetime import datetime, timedelta
+# import time
+# now = datetime.now()
+# print now
+# print now + timedelta(days=1)
+# print now.strftime("%A %d %b %Y %H-%M-%S")
